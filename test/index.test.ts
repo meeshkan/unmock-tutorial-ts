fd47fcc25cf5bb29a6d7330965d19eda50989cf4
Mike Solomon <mike@mikesolomon.org>
2019-01-07 10:45:39 +0200

    Fixes calling of test code

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,10 @@
 import { getProject } from "../src/";
 
+import { kcomnu, unmock } from "unmock";
+
+beforeEach(async () => await unmock()); // <- start unmock
+afterEach(async () => await kcomnu()); // <- end unmock
+
 test("get project", async () => {
   process.env.API_KEY = "u_n_m_o_c_k_200";
   const project = await getProject(123);
