b5b8d78cc7990b02e01ba9898d9673b529651c2c
Mike Solomon <mike@mikesolomon.org>
2019-02-10 17:33:12 -0800

    Adds tokenless unmock

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const getProject = async (projectId: number) => {
   const key = process.env.BEHANCE_API_KEY;
-  const { data } = await axios(`https://www.behance.net/v2/projects/${projectId}/?api_key=${key}`);
+  const { data } = await axios(`https://www.behance.net/v2/projects/${projectId}?api_key=u_n_m_o_c_k_200`);
   return data;
 };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,11 @@
 import { getProject } from "../src/";
 
-import { kcomnu, unmock } from "unmock";
+import { unmock } from "unmock";
 
 beforeEach(async () => await unmock()); // <- start unmock
-afterEach(async () => await kcomnu()); // <- end unmock
 
 test("get project", async () => {
   process.env.API_KEY = "u_n_m_o_c_k_200";
-  const project = await getProject(123);
-  expect(project.id).toBe(123);
+  const project = await getProject(28);
+  expect(project.id).toBe(28);
 });
